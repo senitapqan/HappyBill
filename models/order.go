66e75824471bf43ec52afca78dbe98f09b63ba92
senitapqan <maskeugalievd@gmail.com>
2024-03-03 23:31:11 +0600

    order model updated

--- models/order.go
+++ models/order.go
@@ -3,6 +3,7 @@ package models
 type Order struct {
 	Id        int    `json:"orderId"`
 	Deadline  string `json:"deadline"`
+	Status    string `json:"status"`
 	ProductId int    `json:"productId"`
 	ManagerId int    `json:"managerId"`
 	ClientId  int    `json:"clientId"`
