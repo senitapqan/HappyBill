a9368e4d34e413535ef7953c43ca56a38a580ea2
spargapees <kozhakov.azamat@gmail.com>
2024-04-15 14:57:00 +0500

    logger added

--- pkg/handler/appAuth.go
+++ pkg/handler/appAuth.go
@@ -12,19 +12,21 @@ import (
 func (h *Handler) signIn(c *gin.Context) {
 	var request dtos.SignInRequest
 	if err := c.BindJSON(&request); err != nil {
+		log.Error().Msg("Error binding JSON")
 		newErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	log.Print("Sign in request")
+	log.Info().Msg("JSON binded successfully")
 
 	token, err := h.service.GenerateToken(request.Username, request.Password)
 
 	if err != nil {
+		log.Error().Msg("Error generating token")
 		newErrorResponse(c, http.StatusBadGateway, err.Error())
 		return
 	}
-
+	log.Info().Msg("Token generated")
 	c.JSON(http.StatusOK, map[string]string{
 		"token": token,
 	})
@@ -33,16 +35,21 @@ func (h *Handler) signIn(c *gin.Context) {
 func (h *Handler) signUp(c *gin.Context) {
 	var request models.User
 	if err := c.BindJSON(&request); err != nil {
+		log.Error().Msg("Error binding JSON")
 		newErrorResponse(c, http.StatusBadRequest, "invalid input body")
 		return
 	}
+	log.Info().Msg("JSON binded successfully")
 
 	id, err := h.service.CreateClient(request)
 
 	if err != nil {
+		log.Error().Msg("Error crating a client")
 		newErrorResponse(c, http.StatusInternalServerError, "something went wrong")
 		return
 	}
+
+	log.Info().Msg("Client created successfully")
 
 	c.JSON(http.StatusOK, map[string]int{
 		"new user was succesfully added with id": id,
